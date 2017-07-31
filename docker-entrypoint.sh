ed4a426da534bed05fe7ea8e7ea02ec6638c9cc6
Pan Luo <pan.luo@ubc.ca>
2017-07-31 01:36:54 -0700

    Add # of workers and set 8000 as default Parsoid port

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,13 +2,15 @@
 
 MEDIAWIKI_API_URL="${MEDIAWIKI_API_URL:-http://localhost/w/api.php}"
 MEDIAWIKI_URL="${MEDIAWIKI_URL:-http://localhost}"
-PARSOID_URL="${PARSOID_URL:-http://localhost:8142}"
-#NUM_WORKERS="${NUM_WORKERS:-4}"
+PARSOID_URL="${PARSOID_URL:-http://localhost:8000}"
+PARSOID_DOMAIN="${PARSOID_DOMAIN:-localhost}"
+NUM_WORKERS="${NUM_WORKERS:-4}"
 
 cat /usr/local/lib/node_modules/restbase/config.example.yaml \
     | sed -e "s@http://localhost/w/api.php@${MEDIAWIKI_API_URL}@" \
     | sed -e "s@http://localhost:8142@${PARSOID_URL}@" \
-    | sed -e "s@{domain:localhost}@{domain:$MEDIAWIKI_URL}@" \
+    | sed -e "s@{domain:localhost}@{domain:$PARSOID_DOMAIN}@" \
     > /config.yaml
+echo "num_workers: $NUM_WORKERS" >> /config.yaml
 
 exec "$@"
